a0b59d08067f52dcddbadc3c91865549f0d44abb
davide-rullo <davide.rullo01@gmail.com>
2023-12-05 16:21:57 +0100

    add migration and seed

---
+++ database/migrations/2023_12_05_143700_create_restaurants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 50)->unique();
+            $table->string('address')->nullable();
+            $table->string('vat_number')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
