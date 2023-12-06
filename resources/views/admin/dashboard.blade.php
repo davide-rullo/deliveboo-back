d1042911b6f18df6adc314a7d3a2d0498b3e4874
MatteoNocera <134152768+MatteoNocera@users.noreply.github.com>
2023-12-06 12:27:58 +0100

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,8 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-            Dashboard
-        </h2>
-    @endsection
+    <div class=" vh-100 back_image">
+
+        <div class="container d-flex justify-content-end my-4">
+            <a class="btn btn-outline-dark" href="#"><i class="fa-solid fa-utensils"></i>
+                See Your Restaurants </a>
+        </div>
+    </div>
+@endsection
