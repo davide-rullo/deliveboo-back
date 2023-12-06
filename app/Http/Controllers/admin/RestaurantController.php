1b9a04f22c7689d28955cfb7708e065ed7d40673
Fabio Costantino <fabiocostantino84@gmail.com>
2023-12-06 17:36:28 +0100

    add types on form

--- app/Http/Controllers/admin/RestaurantController.php
+++ app/Http/Controllers/admin/RestaurantController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Models\Restaurant;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Type;
 
 class RestaurantController extends Controller
 {
@@ -34,7 +35,10 @@ class RestaurantController extends Controller
      */
     public function create()
     {
-        return view('admin.restaurants.create');
+
+        $types = Type::all();
+
+        return view('admin.restaurants.create', compact('types'));
     }
 
     /**
