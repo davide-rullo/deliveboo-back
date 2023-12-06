55d8a7562c4b7f0112bfd77fece717bbff4ffdc5
matteoNapoli42 <matteon4242@gmail.com>
2023-12-06 11:36:56 +0100

    updateRestaurantRequest, add method for slug, Restaurant CRUD

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -12,6 +13,11 @@ class Restaurant extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'name', 'address', 'vat_number', 'logo', 'slug', 'phone'];
+
+    public static function generateSlug($name)
+    {
+        return Str::slug($name, '-');
+    }
 
     public function user(): BelongsTo
     {
