cbc6ee8d94ac34ac894419fe5552cac241239080
matteoNapoli42 <matteon4242@gmail.com>
2023-12-14 10:01:10 +0100

    add advanced filtered types search

--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,7 @@ Route::get('/selected', [RestaurantController::class, 'selected']);
 /* Route::get('/types{}') */
 
 Route::get('/types', [TypeController::class, 'index']);
-Route::get('/types/{type:slug}', [TypeController::class, 'show']);
-/* Route::get('/types/selected_types', [TypeController::class, 'show']); */
+Route::post('/types/selected_types', [TypeController::class, 'show']);
 
 Route::get('restaurants/{restaurant:slug}', [RestaurantController::class, 'show']);
 
