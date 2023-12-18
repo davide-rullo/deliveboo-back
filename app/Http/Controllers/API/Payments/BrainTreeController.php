f75e0ba5cb6012aa7a1d6d7a2a4b090df39189b5
matteoNapoli42 <matteon4242@gmail.com>
2023-12-18 11:09:26 +0100

    add payment logic

--- app/Http/Controllers/API/Payments/BrainTreeController.php
+++ app/Http/Controllers/API/Payments/BrainTreeController.php
@@ -10,8 +10,14 @@ use App\Http\Requests\Payments\PaymentRequest;
 class BrainTreeController extends Controller
 {
     //Generazione del token per autorizzare il pagamento
-    public function generate(Request $request, Gateway $gateway)
+    public function generate(Request $request)
     {
+        $gateway = new Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => 'mtzf6rx8gksx2k3v',
+            'publicKey' => 'gbkd3mbpwhp65tp9',
+            'privateKey' => '1f572d2f9210c941ba7d9875e960b627'
+        ]);
         $token = $gateway->clientToken()->generate();
         $data = [
             'success' => true,
@@ -21,8 +27,15 @@ class BrainTreeController extends Controller
     }
 
 
-    public function makePayment(PaymentRequest $request, Gateway $gateway)
+    public function makePayment(PaymentRequest $request)
     {
+        $gateway = new Gateway([
+            'environment' => env('BRAINTREE_ENVIRONMENT'),
+            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+            'privateKey' => env('BRAINTREE_PRIVATE_KEY')
+        ]);
+
 
         $result = $gateway->transaction()->sale([
             'amount' => $request['paymentData']['amount'],
@@ -31,6 +44,7 @@ class BrainTreeController extends Controller
                 'submitForSettlement' => true,
             ]
         ]);
+
         if ($result->success) {
             $data = [
                 'success' => true,
@@ -42,30 +56,7 @@ class BrainTreeController extends Controller
                 'success' => false,
                 'message' => "La transazione è fallita",
             ];
-            return response()->json($data, 401);
+            return response()->json($result, 401);
         }
-
-        /* 
-        $result = $gateway->transaction()->sale([
-            'amount' => $request->amount,
-            'paymentMethodNonce' => $request->token,
-            'options' => [
-                'submitForSettlement' => true,
-            ]
-        ]);
-
-        if ($result->success) {
-            $data = [
-                'success' => true,
-                'message' => 'La transazione è andata a buon fine'
-            ];
-            return response()->json($data, 200);
-        } else {
-            $data = [
-                'success' => false,
-                'message' => "La transazione è fallita",
-            ];
-            return response()->json($data, 401);
-        } */
     }
 }
